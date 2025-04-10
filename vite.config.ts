ae55344967a64b2bc702f422e366da207edff4c2
Allen Jiang <allenjiang17@gmail.com>
2025-04-10 00:01:50 -0400

    adjusted vite conifg

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tennis-sim', // important for GitHub Pages
+  base: '/tennis-sim/', // important for GitHub Pages
 
 })
